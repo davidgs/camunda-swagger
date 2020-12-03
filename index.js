a07f80ea27acdf78e209ac09baf469beaa01b72e
David G. Simmons <santafen@mac.com>
2020-12-03 13:47:52 -0500

    listen on all addresses

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/open
 var app = expressAppConfig.getApp();
 
 // Initialize the Swagger middleware
-http.createServer(app).listen(serverPort, function () {
+http.createServer(app).listen(serverPort, "0.0.0.0", function () {
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
 });
